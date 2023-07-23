758045f1a7670556efad979a1ad4ca064b27d2ad
ahmetgursel <ahmetgursel@protonmail.com>
2023-07-23 23:42:45 +0300

    Add createNewOrganization endpoint to OrganizationController

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiResponse } from '@nestjs/swagger';
 
 @Controller('organizations')
-export class OrganizationController {}
+export class OrganizationController {
+  @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Organization created successfully',
+  })
+  @ApiBadRequestResponse({ description: 'Organization already exists' })
+  createNewOrganization() {
+    return 'createNewOrganization';
+  }
+}
