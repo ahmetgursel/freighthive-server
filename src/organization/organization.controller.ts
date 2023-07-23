04100823c2d36313234aadefd85bbddbddb470c0
ahmetgursel <ahmetgursel@protonmail.com>
2023-07-24 01:03:14 +0300

    add createNewOrganization method to the OrganizationController and OrganizationService

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { CreateOrganizationDto } from './dto';
+import { OrganizationService } from './organization.service';
 
+@ApiTags('Organizations')
+@ApiBearerAuth()
+@UseGuards(JwtGuard)
 @Controller('organizations')
 export class OrganizationController {
+  constructor(private organization: OrganizationService) {}
+
   @Post()
   @ApiResponse({
     status: 201,
     description: 'Organization created successfully',
   })
   @ApiBadRequestResponse({ description: 'Organization already exists' })
-  createNewOrganization() {
-    return 'createNewOrganization';
+  createNewOrganization(
+    @Body() dto: CreateOrganizationDto,
+    @GetUser('id') userId: string,
+  ) {
+    return this.organization.createNewOrganization(dto, userId);
   }
 }
--- src/organization/organization.service.ts
+++ src/organization/organization.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateOrganizationDto } from './dto';
 
 @Injectable()
-export class OrganizationService {}
+export class OrganizationService {
+  constructor(private prisma: PrismaService) {}
+
+  createNewOrganization(dto: CreateOrganizationDto, userId: string) {
+    try {
+      const organization = this.prisma.organization.create({
+        data: {
+          name: dto.name,
+          address: dto.address,
+          taxNumber: dto.taxNumber,
+          taxOffice: dto.taxOffice,
+          invoiceAddress: dto.invoiceAddress,
+          createdById: userId,
+        },
+      });
+
+      return organization;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Organization already exists');
+        }
+      }
+      throw error;
+    }
+  }
+}
