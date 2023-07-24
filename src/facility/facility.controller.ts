a2990980e5d83236aa99329157e8236b8b52d9e0
ahmetgursel <ahmetgursel@protonmail.com>
2023-07-25 02:45:15 +0300

    Add createNewFacility API endpoint

--- src/facility/facility.controller.ts
+++ src/facility/facility.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { CreateFacilityDto } from './dto';
+import { FacilityService } from './facility.service';
 
+@ApiTags('Facility')
+@ApiBearerAuth()
+@UseGuards(JwtGuard)
 @Controller('facility')
-export class FacilityController {}
+export class FacilityController {
+  constructor(private facility: FacilityService) {}
+
+  @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Facility created successfully',
+  })
+  @ApiBadRequestResponse({ description: 'Facility already exists' })
+  createNewFacility(
+    @Body() dto: CreateFacilityDto,
+    @GetUser('id') userId: string,
+  ) {
+    return this.facility.createNewFacility(dto, userId);
+  }
+}
--- src/facility/facility.service.ts
+++ src/facility/facility.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateFacilityDto } from './dto';
 
 @Injectable()
-export class FacilityService {}
+export class FacilityService {
+  constructor(private prisma: PrismaService) {}
+
+  createNewFacility(dto: CreateFacilityDto, userId: string) {
+    try {
+      const facility = this.prisma.facility.create({
+        data: {
+          name: dto.name,
+          address: dto.address,
+          city: dto.city,
+          country: dto.country,
+          createdById: userId,
+        },
+      });
+
+      return facility;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Facility already exists');
+        }
+      }
+      throw error;
+    }
+  }
+}
