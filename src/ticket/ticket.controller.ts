4ae8daaba5d162136d9f6d95213e6bf5d25cae18
ahmetgursel <ahmetgursel@protonmail.com>
2023-07-26 02:27:21 +0300

    Add createNewTicket method to TicketController and TicketService

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { CreateTicketDto } from './dto';
+import { TicketService } from './ticket.service';
 
-@Controller('ticket')
-export class TicketController {}
+@ApiTags('Tickets')
+@ApiBearerAuth()
+@UseGuards(JwtGuard)
+@Controller('tickets')
+export class TicketController {
+  constructor(private ticket: TicketService) {}
+
+  @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Tickets created successfully',
+  })
+  @ApiBadRequestResponse({ description: 'Container number already exists' })
+  createNewTicket(@Body() dto: CreateTicketDto, @GetUser('id') userId: string) {
+    return this.ticket.createNewTicket(dto, userId);
+  }
+}
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTicketDto } from './dto';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+  constructor(private prisma: PrismaService) {}
+
+  async createNewTicket(dto: CreateTicketDto, userId: string) {
+    try {
+      const ticket = this.prisma.ticket.create({
+        data: {
+          containerNumber: dto.containerNumber,
+          entryTime: dto.entryTime,
+          exitTime: dto.exitTime,
+          truckId: dto.truckId,
+          organizationId: dto.organizationId,
+          facilityId: dto.facilityId,
+          isInvoiceCreated: dto.isInvoiceCreated,
+          createdById: userId,
+        },
+      });
+
+      return ticket;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Container number already exists');
+        }
+      }
+      throw error;
+    }
+  }
+}
